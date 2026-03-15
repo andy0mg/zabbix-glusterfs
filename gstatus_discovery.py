4af6f1a03558a4d3fd36ff16071d53096ae64537
andy0mg <40331027+andy0mg@users.noreply.github.com>
2026-03-15 21:07:54 +0300

    Update gstatus_discovery.py

--- gstatus_discovery.py
+++ gstatus_discovery.py
@@ -4,31 +4,95 @@ import subprocess
 import sys
 
 gluster_volume_names = []
-gstatus_output = subprocess.check_output('gstatus -a -o json ', shell=True).decode()
-date, time, json_part = gstatus_output.split(maxsplit=2)
-gluster_info = json.loads(json_part)
-volume_list = gluster_info["volume_summary"]
+
+def fail():
+    if len(sys.argv) == 1:
+        print(json.dumps({"data": []}))
+    elif len(sys.argv) == 3 and sys.argv[1] == "state":
+        print("down")
+    else:
+        print(0)
+    sys.exit(1)
+
+try:
+    gstatus_output = subprocess.check_output(
+        ['/usr/bin/gstatus', '-a', '-o', 'json'],
+        stderr=subprocess.STDOUT,
+        timeout=30
+    ).decode('utf-8', errors='replace')
+except Exception:
+    fail()
+
+json_start = gstatus_output.find('{')
+json_end = gstatus_output.rfind('}')
+if json_start == -1 or json_end == -1 or json_end < json_start:
+    fail()
+
+json_part = gstatus_output[json_start:json_end + 1]
+
+try:
+    gluster_info = json.loads(json_part)
+except Exception:
+    fail()
+
+data = gluster_info.get("data", {})
+volume_list = data.get("volume_summary", [])
 
 nargs = len(sys.argv)
 
 if nargs == 1:
     for volume in volume_list:
-        gluster_volume_names.append({"{#VOLUME_NAME}": volume["volume_name"]})
-    print(json.dumps({'data': gluster_volume_names}))
+        if volume.get("name"):
+            gluster_volume_names.append({"{#VOLUME_NAME}": volume["name"]})
+    print(json.dumps({"data": gluster_volume_names}))
 
 elif nargs == 2:
-    print(gluster_info[sys.argv[1]])
+    key = sys.argv[1]
+
+    if key == "heal_entries":
+        total = 0
+        for volume in volume_list:
+            for item in volume.get("healinfo", []):
+                try:
+                    total += int(item.get("nr_entries", 0))
+                except (TypeError, ValueError):
+                    pass
+        print(total)
+
+    elif key == "heal_active":
+        total = 0
+        for volume in volume_list:
+            for item in volume.get("healinfo", []):
+                try:
+                    total += int(item.get("nr_entries", 0))
+                except (TypeError, ValueError):
+                    pass
+        print(1 if total > 0 else 0)
+
+    else:
+        value = data.get(key, 0)
+        if value in (None, ''):
+            print(0)
+        else:
+            print(value)
+
 elif nargs == 3:
     for volume in volume_list:
-        if volume.get('volume_name') and sys.argv[2] == volume["volume_name"]:
-            print(volume[sys.argv[1]])
+        if volume.get("name") and sys.argv[2] == volume["name"]:
+            value = volume.get(sys.argv[1], 0)
+            if value in (None, ''):
+                if sys.argv[1] == "state":
+                    print("down")
+                else:
+                    print(0)
+            else:
+                print(value)
             break
     else:
         if sys.argv[1] == "state":
-            print('down')
+            print("down")
         else:
-            print()
-        
-else:
-    print('Wrong arguments')
+            print(0)
 
+else:
+    print("Wrong arguments")
